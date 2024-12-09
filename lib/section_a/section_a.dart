57f01978f42cbb81eb22b84c0e48eb02b7857e7e
Faeith Ahab <faeithahab@gmail.com>
2024-12-09 17:28:19 +0300

    a

--- lib/section_a/section_a.dart
+++ lib/section_a/section_a.dart
@@ -9,3 +9,24 @@
 //  Flutter enables integration with platform-specific features through plugins and platform channels.
 
 //c
+//  Dart is an open-source language programming language developed by Google. It is utilised in application development on both the client and server sides using Flutter.
+//  Reasons:
+//  Dart can compile both ahead-of-time (AOT) for enhanced performance and just-in-time (JIT) for faster development cycles.
+//  Dart has a major advantageous support for synchronous programming with functionality like async and await, which enables it to handlle tasks like file operations, and network requests effectively.
+//  Dart comes along with vast libraries and tools that are intergrated with flutter.
+
+//d
+// Hot reloading enables a developer to view changes whilst the app is running. It syncs newer versions of the files that you edited at runtime upon pressing the r button shortcut.. It leverages HMR and without reloading the page.
+// It replaces changed components in-place while preserving state, which enables quick debugging, and instant visualisation of updated code-base.
+
+//e
+//  Variety of pre-built  widgets to choose from, which enhances customisation.
+//  Cross-Platform Development, a singlular codebase for both Android and iOS which saves on resources.
+//  Flutter compiles to native machine code, which is similar to a native app built independentlyfor each platform.
+
+//f
+// A stateful widget is dynamic by nature. It's appearance in changed in response to events triggered by user interactions or any other factors.
+// Eg. Checkbox that changes its state when clicked
+// A steless widget is static in nature. Once created, the properties cannot change.
+
+//g
